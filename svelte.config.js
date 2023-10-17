5752e1a2eb40368e0fb107ae1a3ee3d690e11126
itsrusty <rusty3031@gmail.com>
2023-10-16 20:44:38 -0700

    fix a bug with svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import {preprocessMeltUI} from "@melt-ui/pp";
-import sequence from "svelte-sequential-preprocessor";
 import adapter from '@sveltejs/adapter-auto';
 import {vitePreprocess} from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config}*/
