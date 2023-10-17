4b44926e0ed1c9dffbbed06fb76ca6d60c64d8e4
itsrusty <rusty3031@gmail.com>
2023-10-16 20:58:20 -0700

    fixed responsive mobile

--- src/+layout.svelte
+++ src/+layout.svelte
@@ -1,4 +1,12 @@
 <script>
     import "./app.css"
+    import { onMount } from "svelte";
+
+    onMount(()=>{
+        window.location.replace("/login")
+    })
 </script>
 <slot/>
+
+
+<p>redireccionandote guapo(a)</p>
